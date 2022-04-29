6dd0bf792a6e563474c9b4f0770a947d91876371
EzoRisu <ucccblack@gmail.com>
2022-04-30 08:34:14 +0900

    ２回め

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    var image1: UIImage!
+    
+    @IBOutlet weak var ZoomButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var playstopButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        image1 = UIImage(named: "airplane-3702676_1280.jpg")
+        
         // Do any additional setup after loading the view.
+        
+        imageView.image = image1
     }
 
-
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        
+    }
 }
 
